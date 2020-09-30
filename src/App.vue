e951661501ccc1624abe7c8bd0960fb78d1ce69b
Yeansovanvathana <61142466+Yeansovanvathana@users.noreply.github.com>
2020-09-30 23:52:26 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Navbar/>
+    <hr>
+    <Body/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Body from './components/Body.vue';
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Body,
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  background-image: url('./assets/Info_page.jpg');
+  background-size: cover;
+  height: 445px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <b-navbar toggleable type="light" variant="white">
+    <b-navbar-brand tag="h1" href="#">Unibox</b-navbar-brand>
+      <b-navbar style="padding-right:100px;">
+        <b-navbar-item style="padding-right:70px;" href="#">FAQ</b-navbar-item>
+        <b-navbar-item style="padding-right:70px;" href="#">AboutUs</b-navbar-item>
+        <b-navbar-item style="padding-right:70px;" href="#">Download</b-navbar-item>
+      </b-navbar>
+  </b-navbar>
+</div>
+</template>
+<style>
+
+
+</style>
